faeccf7ed1e180dc0c7be9c9713f4a6909fdb532
vishnu <vishnumelayil8@gmail.com>
2020-04-20 15:22:23 +0530

    Exchange service

---
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({ "com.example.currencyexchangeservice*","com.example.currencyexchangeservice.repository"})
+
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}

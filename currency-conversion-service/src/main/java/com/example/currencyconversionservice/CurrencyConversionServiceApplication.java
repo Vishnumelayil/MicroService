06b32119401f7175a9dfd95563e0c4ed929f3dfa
vishnu <vishnumelayil8@gmail.com>
2020-04-20 15:23:21 +0530

    conversion service updated with feign clinet temlate

---
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableFeignClients("com.example.currencyconversionservice.*")
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
